52fcecb673f1600c2986a2df3e88e478b0554fae
Nzabinesha <m.nzabinesh@alustudent.com>
2025-06-28 09:12:17 +0000

    this is updated top

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,26 +1,5 @@
-#!/on to print the top 10 hot posts of a subreddit"""
-import requests
 
-
-def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts for a subreddit"""
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'ALU-API-Project/1.0'}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
-        print(None)
-        return
-
-    data = response.json()
-    posts = data.get('data', {}).get('children', [])
-
-    for post in posts:
-        print(post.get('data', {}).get('title'))
-in/python3
-"""
-Function that queries the Reddit API and prints the titles
+n that queries the Reddit API and prints the titles
 of the first 10 hot posts listed for a given subreddit.
 """
 
