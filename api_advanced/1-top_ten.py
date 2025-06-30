ee92a8b85b8d89e7b6ed5e95e1f004890fade19f
Nzabinesha <m.nzabinesh@alustudent.com>
2025-06-30 23:14:25 +0000

    upgraded top ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python3
+"""
+1-top_ten.py
+
+Fetches and prints the titles of the top 10 hot posts from a given subreddit
+using the Reddit API. If the subreddit is invalid, it prints None.
+
+Author: Nzabinesha Merci
+Date: June 2025
+"""
 
 import requests
 
+
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts of a subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    """
+    Prints the titles of the first 10 hot posts for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        None: Prints each title or None if subreddit is invalid.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'SubredditTopTenViewer/1.0'}
 
     try:
@@ -18,9 +36,7 @@ def top_ten(subreddit):
         posts = data.get("data", {}).get("children", [])
 
         for post in posts:
-            title = post.get("data", {}).get("title")
-            print(title)
+            print(post.get("data", {}).get("title"))
 
     except Exception:
         print(None)
-
